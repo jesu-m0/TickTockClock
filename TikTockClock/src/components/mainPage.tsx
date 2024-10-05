d31e687a2b24af0c67b9c59179968203d0391336
jesu-m0 <jesusmd2000@gmail.com>
2024-10-05 03:55:23 +0200

    medfiaQUERIES NOT EWORJKKUING

--- TikTockClock/src/components/mainPage.tsx
+++ TikTockClock/src/components/mainPage.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import './MainPage.css';
 
 const MainPage: React.FC = () => {
   return (
-    <p>hoal</p>
+    <div className='mbView md:mdView lg:lgView'>
+        <div className='aspect-[1/1] card xsCard md:sCard lg:lgCard bg-eerieBlack'>
+            <p className='text-white md:text-red-500 lg:text-blue-400'>pedro Sanchez</p>
+        </div>
+    </div>
   );
 };
 
